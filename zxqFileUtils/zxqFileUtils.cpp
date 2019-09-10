7b4863e67af2ec7888c6750a0748ccaa196e9b54
xueqianz <xueqian.zhang@intel.com>
2019-09-10 13:43:30 +0800

    jpeg implement

--- zxqFileUtils/zxqFileUtils.cpp
+++ zxqFileUtils/zxqFileUtils.cpp
@@ -6,10 +6,11 @@
 #include <fstream>
 
 void readBMP(const char* fileName);
+void readJPEG();
 
 int main()
 {
-	readBMP("123");
+	readJPEG();
 }
 
 void readBMP(const char* fileName) {
@@ -50,18 +51,25 @@ void readBMP(const char* fileName) {
 	std::cout  << info.biSize << " " << info.biClrImportant;
 }
 
+unsigned short u_shortRevert(unsigned short num) {
+	return ((num & 0xff) << 8) | ((num & 0xff00) >> 8);
+}
+
 void readJPEG() {
 	std::ifstream file("C:/Users/xueqianz/Desktop/workspace/CppProjects/bmp_files/24_bit_color.jpg", std::ios::binary);
-	const char JPEG_SOI = 0xd8;
-	const char JPEG_APP0 = 0xe0;
-	const char JPEG_DQT = 0xdb;
-	const char JPEG_SOF0 = 0xc0;
-	const char JPEG_DHT = 0xc4;
-	const char JPEG_DRI = 0xdd;
-	const char JPEG_SOS = 0xda;
-	const char JPEG_EOI = 0xd9;
+
+	const unsigned char JPEG_SOI = 0xd8;
+	const unsigned char JPEG_APP0 = 0xe0;
+	const unsigned char JPEG_APP1 = 0xe1;
+	const unsigned char JPEG_APP15 = 0xef;
+	const unsigned char JPEG_DQT = 0xdb;
+	const unsigned char JPEG_SOF0 = 0xc0;
+	const unsigned char JPEG_DHT = 0xc4;
+	const unsigned char JPEG_DRI = 0xdd;
+	const unsigned char JPEG_SOS = 0xda;
+	const unsigned char JPEG_EOI = 0xd9;
+
 	typedef struct zJPEGAPP0INFO {
-		unsigned short jaSize;
 		unsigned short flagBits1;
 		unsigned short flagBits2;
 		unsigned short restFlagBits_versionLowBits;
@@ -70,14 +78,18 @@ void readJPEG() {
 		unsigned short yDensity;
 		unsigned short thumbDensity;
 	} JPEGAPP0INFO;
+
 	typedef struct zJPEGAPPnINFO {
 		unsigned short jaSize;
 	} JPEGAPPnINFO;
 
+	JPEGAPP0INFO app0;
+	unsigned char* dqt;
+
 	bool isFlag = false;
-	char item;
+	unsigned char item;
 	while (!file.eof()) {
-		file.read(&item, sizeof(char));
+		file.read((char*)&item, sizeof(unsigned char));
 		if (item == 0xff) {
 			isFlag = true;
 			continue;
@@ -86,21 +98,45 @@ void readJPEG() {
 			switch (item)
 			{
 			case JPEG_SOI:
+				std::cout << "SOI" << std::endl;
+				break;
+			case JPEG_APP0:
+				std::cout << "APP0" << std::endl;
+				unsigned short app0Size;
+				file.read((char*)&app0Size, sizeof(unsigned short));
+				app0Size = u_shortRevert(app0Size);
+				file.read((char*)&app0, sizeof(JPEGAPP0INFO));
+				std::cout << std::hex << app0.flagBits1 << app0.flagBits2 << std::endl;
 				break;
 			case JPEG_DQT:
+				std::cout << "DQT" << std::endl;
+				unsigned short dqtSize;
+				file.read((char*)&dqtSize, sizeof(unsigned short));
+				dqtSize = u_shortRevert(dqtSize);
+				dqt = (unsigned char*)malloc(sizeof(char)*(dqtSize - 2));
+				file.read((char*)dqt, sizeof(char)*(dqtSize - 2));
+				std::cout << std::hex << dqt[0] << std::endl;
 				break;
 			case JPEG_SOF0:
+				std::cout << "SOF0" << std::endl;
 				break;
 			case JPEG_DHT:
+				std::cout << "DHT" << std::endl;
 				break;
 			case JPEG_DRI:
+				std::cout << "DRI" << std::endl;
 				break;
 			case JPEG_SOS:
+				std::cout << "SOS" << std::endl;
 				break;
 			case JPEG_EOI:
+				std::cout << "EOI" << std::endl;
 				break;
 			default:
-				if (item > 0xe0 && item <= 0xef) {
+				if (item >= JPEG_APP1 && item <= JPEG_APP15) {
+
+				}
+				else if(item == 0x00) {
 
 				}
 				else {
@@ -109,6 +145,8 @@ void readJPEG() {
 				break;
 			}
 		}
+
+		isFlag = false;
 	}
 }
 
